c9e402c8f2c18466f8751b44d6d8806ea9a4e92c
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2024-05-04 22:39:45 +0800

    feat: update skills and projects

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,20 +2,25 @@
 	import Table from '../../components/Table.svelte';
 	import { TabItem, Tabs } from 'flowbite-svelte';
 	let courseworkItems: string[][] = [
-		['CS1101S Programming Methodology I', 'Year 1 Semester 1'],
-		['CS1231S Discrete Structures', 'Year 1 Semester 1'],
-		['CS2030S Programming Methodology II', 'Year 1 Semester 2'],
-		['CS2040S Data Structures and Algorithms', 'Year 1 Semester 2'],
-		['CS2100 Computer Organisation', 'Ongoing'],
-		['CS2105 Introduction to Computer Networks', 'Ongoing'],
-		['CS2109S Introduction to AI and Machine Learning', 'Ongoing'],
-		['CS3241 Computer Graphics', 'Ongoing'],
-		['CS3230 Design and Analysis of Algorithms', 'Ongoing']
+		['CS1101S Programming Methodology I', 'Y1S1'],
+		['CS1231S Discrete Structures', 'Y1S1'],
+		['CS2030S Programming Methodology II', 'Y1S2'],
+		['CS2040S Data Structures and Algorithms', 'Y1S2'],
+		['CS2100 Computer Organisation', 'Y2S1'],
+		['CS2105 Introduction to Computer Networks', 'Y2S1'],
+		['CS2109S Introduction to AI and Machine Learning', 'Y2S1'],
+		['CS3241 Computer Graphics', 'Y2S1'],
+		['CS3230 Design and Analysis of Algorithms', 'Y2S1'],
+		['CS2101 Effective Communication for Computing Professionals', 'Y2S2'],
+		['CS2102 Database Systems', 'Y2S2'],
+		['CS2103T Software Engineering', 'Y2S2'],
+		['CS2106 Introduction to Operating Systems', 'Y2S2'],
+		['CS2107 Introduction to Information Security', 'Y2S2'],
 	];
 
 	let skillItems: string[][] = [
 		['Ruby on Rails', 'Backend'],
-		['Golang', 'Backend programming language'],
+		['Golang', 'Programming language'],
 		['TypeScript', 'Programming language'],
 		['React', 'Frontend framework'],
 		['MySQL', 'Database language'],
@@ -39,7 +44,8 @@
 		['Flowbite Svelte', 'Component library'],
 		['GORM', 'ORM library'],
 		['REST API', 'API'],
-		['Phaser 3', 'Game framework']
+		['Phaser 3', 'Game framework'],
+		['JavaFX', 'Java library']
 	];
 </script>
 
@@ -47,14 +53,13 @@
 	<Tabs>
 		<TabItem open title="Relevant Coursework">
 			<p class="font-mormal m-8 whitespace-pre-line text-sm text-gray-700 dark:text-gray-400">
-				As a computer science sophomore, here are some relevant courses I have taken, or am
-				currently taking.
+				As a computer science sophomore, here are some relevant courses I have taken.
 			</p>
 			<Table headers={['Relevant Coursework', 'Taken in']} items={courseworkItems} />
 		</TabItem>
 		<TabItem title="Skills">
 			<p class="font-mormal m-8 whitespace-pre-line text-sm text-gray-700 dark:text-gray-400">
-				Here some technologies that I have worked with before.
+				Here are some technologies that I have worked with before.
 			</p>
 
 			<Table headers={['Skills', 'Description']} items={skillItems} />
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,8 +3,14 @@
 </script>
 
 <div class="grid gap-4 md:grid-cols-2">
+	<ProjectCard title="TAPro" link="https://github.com/AY2324S2-CS2103T-F13-1/tp">
+		TAPro is a contact book application for Computer Science TAs. Created using Java and JavaFX, in a team of 5 for CS2103T.
+	</ProjectCard>
+	<ProjectCard title="Stratify" link="https://github.com/tituschewxj/ip/">
+		Stratify is a task management chat-bot. Created using Java and JavaFX, for CS2103T.
+	</ProjectCard>
 	<ProjectCard title="CVWO Assignment" link="https://github.com/tituschewxj/gossip-app">
-		I created a full stack application using a React-Typescript frontend, and a Ruby on Rails
+		Created a full stack application using a React-Typescript frontend, and a Ruby on Rails
 		backend for the Computing for Voluntary Welfare Organisations (CVWO) Assignment.
 	</ProjectCard>
 	<ProjectCard title="Arcade 2D" link="https://github.com/source-academy/modules">
@@ -12,12 +18,13 @@
 		Typescript, React, and Phaser 3.
 	</ProjectCard>
 	<ProjectCard title="Boxes" link="https://github.com/tituschewxj/GMTK-Game-Jam-2023-Boxes">
-		I created a game in the span of 48 hours for the GMTK Game Jam 2023 using Unity.
+		Created a game in the span of 48 hours for the GMTK Game Jam 2023 using Unity.
 	</ProjectCard>
 	<ProjectCard title="Ster" link="https://github.com/tituschewxj/Gamecraft-2022-Game-Jam-Ster">
-		I created a game in a week for the Gamecraft! 2022 Game Jam using Unity.
+		Created a game in a week for the Gamecraft! 2022 Game Jam using Unity.
 	</ProjectCard>
 	<ProjectCard title="Porfolio Website" link="https://github.com/tituschewxj/tituschewxj.github.io">
-		I created this website using Svelte, Tailwind, and the Flowbite Svelte component library.
+		Created this website using Svelte, Tailwind, and the Flowbite Svelte component library.
 	</ProjectCard>
+
 </div>
