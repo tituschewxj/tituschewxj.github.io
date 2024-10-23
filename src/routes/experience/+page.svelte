f06209b597b60b38435036ff8b71f39f89638c46
tituschewxj <tituschewxj@gmail.com>
2024-10-24 01:22:27 +0800

    feat: update experience for 2024

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -4,15 +4,36 @@
 
 <div>
 	<Timeline class="md:m-8 md:max-w-xl">
+		<TimelineItem title="Computing for Voluntary Welfare Organisations" date="May 2024">
+			<p class="whitespace-normal text-sm text-gray-700 dark:text-gray-400">
+				Computing for Voluntary Welfare Organisations (CVWO) is an NUS programme that builds and
+				maintains quality IT systems for Voluntary Welfare Organisations (VWOs).
+			</p>
+			<br />
+			<p class="whitespace-normal text-sm text-gray-700 dark:text-gray-400">
+				As the team lead for Lions Befrienders Service Association (LBSA), I worked on leading a
+				team of 10 to migrate their Active Ageing Centres (AAC) System to CVWO's common AAC system,
+				ensuring maintainability and improving the performance and security of our systems. As
+			</p>
+			<br />
+			<p class="whitespace-normal text-sm text-gray-700 dark:text-gray-400">
+				Organising various site visits and communicating with our partner VWOs, to understand their
+				workflows to ensure that the system supports their needs, which helps our partner VWOs focus
+				on helping their beneficiaries.
+			</p>
+		</TimelineItem>
 		<TimelineItem title="Computing for Voluntary Welfare Organisations" date="May 2023">
 			<p class="whitespace-normal text-sm text-gray-700 dark:text-gray-400">
 				Computing for Voluntary Welfare Organisations (CVWO) is an NUS programme that builds and
-				maintains quality IT systems for Voluntary Welfare Organisations (VWOs). Working in a team
-				of 7, I maintained and improved the Case Management System (CMS). I ported the CMS backend
-				from Ruby on Rails to Golang, resulting in significant performance improvements, as well as
-				long term maintainability of the codebase. I also made bug fixes and enhancements for our
-				partner VWOs, which streamlines their workflow, allowing them to spend their time and energy
-				on helping their beneficiaries instead.
+				maintains quality IT systems for Voluntary Welfare Organisations (VWOs).
+			</p>
+			<br />
+			<p class="whitespace-normal text-sm text-gray-700 dark:text-gray-400">
+				Working in a team of 7, I maintained and improved the Case Management System (CMS). I ported
+				the CMS backend from Ruby on Rails to Golang, resulting in significant performance
+				improvements, as well as long term maintainability of the codebase. I also made bug fixes
+				and enhancements for our partner VWOs, which streamlines their workflow, allowing them to
+				spend their time and energy on helping their beneficiaries instead.
 			</p>
 		</TimelineItem>
 		<TimelineItem title="Source Academy" date="January 2023">
