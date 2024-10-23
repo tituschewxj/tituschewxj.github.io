2c4651fd450fcfe2e4e103cda683d435d9f0dc76
tituschewxj <tituschewxj@gmail.com>
2024-10-24 01:26:36 +0800

    feat: update teaching

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -4,7 +4,7 @@
 
 <div>
 	<Timeline class="md:m-8 md:max-w-xl">
-		<TimelineItem title="Computing for Voluntary Welfare Organisations" date="May 2024">
+		<TimelineItem title="Computing for Voluntary Welfare Organisations - Team Lead" date="May 2024">
 			<p class="whitespace-normal text-sm text-gray-700 dark:text-gray-400">
 				Computing for Voluntary Welfare Organisations (CVWO) is an NUS programme that builds and
 				maintains quality IT systems for Voluntary Welfare Organisations (VWOs).
@@ -13,7 +13,7 @@
 			<p class="whitespace-normal text-sm text-gray-700 dark:text-gray-400">
 				As the team lead for Lions Befrienders Service Association (LBSA), I worked on leading a
 				team of 10 to migrate their Active Ageing Centres (AAC) System to CVWO's common AAC system,
-				ensuring maintainability and improving the performance and security of our systems. As
+				ensuring maintainability and improving the performance and security of our systems.
 			</p>
 			<br />
 			<p class="whitespace-normal text-sm text-gray-700 dark:text-gray-400">
@@ -22,7 +22,13 @@
 				on helping their beneficiaries.
 			</p>
 		</TimelineItem>
-		<TimelineItem title="Computing for Voluntary Welfare Organisations" date="May 2023">
+		<TimelineItem title="Programming Methodology II - Teaching Assistant" date="May 2023">
+			<p class="whitespace-normal text-sm text-gray-700 dark:text-gray-400">
+				As a teaching assistant for this module, I conducted labs sessions, teaching Object Oriented
+				Programming and Functional Programming in Java to a class of 13 students.
+			</p>
+		</TimelineItem>
+		<TimelineItem title="Computing for Voluntary Welfare Organisations - Developer" date="May 2023">
 			<p class="whitespace-normal text-sm text-gray-700 dark:text-gray-400">
 				Computing for Voluntary Welfare Organisations (CVWO) is an NUS programme that builds and
 				maintains quality IT systems for Voluntary Welfare Organisations (VWOs).
@@ -36,7 +42,7 @@
 				spend their time and energy on helping their beneficiaries instead.
 			</p>
 		</TimelineItem>
-		<TimelineItem title="Source Academy" date="January 2023">
+		<TimelineItem title="Source Academy - Module Developer" date="January 2023">
 			<p class="whitespace-normal text-sm text-gray-700 dark:text-gray-400">
 				Source Academy is a platform used by NUS students to learn computational thinking in
 				CS1101S, Programming Methodology I. Working as a pair, I designed and implemented Source
